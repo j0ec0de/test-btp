725c17aadb11ea65101431433daa23b531ec4599
Joel Anto <joel.a@ivldsp.com>
2025-11-11 16:56:14 +0000

    ajax call to get details and sent a payload

--- app/shop/webapp/custom/CustomActions.js
+++ app/shop/webapp/custom/CustomActions.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/BusyIndicator",
-    "sap/m/MessageToast"
-], function (BusyIndicator, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/m/Dialog"
+], function (BusyIndicator, MessageToast, Dialog) {
     "use strict";
  
     return {
@@ -12,6 +13,68 @@ sap.ui.define([
                 BusyIndicator.hide();
                 MessageToast.show("Action completed successfully!");
             }, 2000);
+        //},
+            // another button function
+
+            // show: function () {
+            //     BusyIndicator.show(0);
+
+            // ajax call
+            jQuery.ajax({
+                url:"/odata/v4/shop/Orders",
+                type: "GET",
+                contentType: "application/json",
+
+                success: function (data) {
+                    BusyIndicator.hide();
+                    
+                    // For OData v4, the array of results is in 'data.value'
+                    const orderCount = data.value.length;
+                    MessageToast.show(`AJAX Success! Fetched ${orderCount} orders.`);
+                    
+
+                    // You can loop through the data
+                    console.log("Fetched Orders:", data.value);
+                    data.value.forEach(order => {
+                        console.log(`Order ID: ${order.ID}, Customer: ${order.customerName}`);
+                    });
+                },
+
+                error: function (xhr, status, error) {
+                    BusyIndicator.hide();
+                    MessageBox.error(`AJAX Failed: ${error}`);
+                }
+            })
+        },
+
+        show: function() {
+            BusyIndicator.show(0);
+            
+            // static payload
+
+            const myPayLoad = {
+                "orderID": "OR003",
+            };
+
+            jQuery.ajax({
+                url: "/odata/v4/shop/checkOrderStatus",
+                type: "POST",
+                contentType: "application/json",
+
+                data: JSON.stringify(myPayLoad),
+
+                success: function (data) {
+                    BusyIndicator.hide();
+                    MessageToast.show("POST request successfully!");
+                    console.log("Response:", data);
+                },
+                
+                error: function(error) {
+                    BusyIndicator.hide();
+                    MessageBox.error(`Post action failed: ${error}`);
+                }
+                
+            })
         }
     };
 });
